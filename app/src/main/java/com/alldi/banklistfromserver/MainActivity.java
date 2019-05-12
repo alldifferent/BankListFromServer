6b67dc6f60c122308160252d047ab8b3dccc29b2
somsap <rome777@gmail.com>
2019-05-12 10:36:19 +0900

    okio 라이브러리 추가하여 오류 해결

--- app/src/main/java/com/alldi/banklistfromserver/MainActivity.java
+++ app/src/main/java/com/alldi/banklistfromserver/MainActivity.java
@@ -4,9 +4,13 @@ import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.alldi.banklistfromserver.databinding.ActivityMainBinding;
 import com.alldi.banklistfromserver.utils.ConnectServer;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends BaseActivity {
 
@@ -26,7 +30,34 @@ public class MainActivity extends BaseActivity {
         act.serverTestBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ConnectServer.getRequestInfoBank(mContext, null);
+                
+                ConnectServer.getRequestInfoBank(mContext, new ConnectServer.JsonResponseHandler() {
+                    @Override
+                    public void onResponse(JSONObject json) {
+//                        실제로 서버에서 돌아온 응답을 메인 액티비티에서 처리하는 메쏘드.
+
+                        try {
+                            int code = json.getInt("code");
+
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    if (code == 200){
+                                        Toast.makeText(mContext, "정상적으로 데이터를 가져왔습니다.", Toast.LENGTH_SHORT).show();
+                                    }else {
+                                        Toast.makeText(mContext, "서버통신에 문제가 있습니다.", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                });
+                
             }
         });
     }
